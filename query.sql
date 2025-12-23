547d4c521fdc6172e2ad4bc1945bbc7f0337dde4
mdsamiulislam54 <samiulm5332@gmail.com>
2025-12-23 12:31:07 +0600

    Retrieve booking information along with Customer name and Vehicle name.

---
+++ query.sql
@@ -0,0 +1,15 @@
+-- Query 1
+select
+  b.booking_id,
+  u.name as "customer_name",
+  v.vehicle_name,
+  b.start_date,
+  b.end_date,
+  b.status
+from
+  booking as b
+  inner join users as u on b.user_id = u.user_id
+  inner join vehicle as v on b.vehicle_id = v.vehicle_id
+
+
+
